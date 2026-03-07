1d7794e6f7c97c398becda5c3e2ccb3ad182685d
CodeNur <dodgechalanger9@gmail.com>
2026-03-07 21:28:45 +0500

    reklama

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <section className="animate-fade-in h-full p-5">codenur.uz - deb qidiring!</section>;
+  return (
+    <>
+    <section className="animate-fade-in h-full p-5">codenur.uz - deb qidiring!</section>
+    <section className="animate-fade-in h-full p-5">ilhomlandim.uz - deb qidiring!</section>
+    </>
+  );
 }
