7c132812605d3685f47d22d6d928ce6261d31189
CodeNur <dodgechalanger9@gmail.com>
2026-03-07 20:22:39 +0500

    Add content to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <section className="animate-fade-in h-full p-5"></section>;
+  return <section className="animate-fade-in h-full p-5">Qondaaaay</section>;
 }
